694a30315e39b0141e9e9900c8de9acf98db2e41
Tkumar02 <ta_265@yahoo.co.uk>
2023-08-12 18:16:16 +0100

    swtichCases

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -6,20 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-  postArray: Array<string> = ['Post 1','Post 2','Post 3','Post 4', 'Post 5']
-  postObject: Array<any> = []
-  
-  lastPost: number = 0
-  newTitle: string = ''
-  
-  addPost(){
-    this.lastPost = this.postObject.length + 1
-    this.newTitle = 'Post ' + this.lastPost
-    this.postObject.push({title:this.newTitle, id:this.lastPost})
-  }
-  deletePost(i:number){
-    console.log(i,'this is i')
-    this.postObject.splice(i,1)
-    console.log(this.postObject.length, 'length')
-  }
+  form: string = ''
+  changeForm(status:string){
+    this.form = status
+    console.log(this.form)
+  } 
 }
