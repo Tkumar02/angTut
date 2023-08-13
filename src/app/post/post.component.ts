ceeaebdda601b3e23bc7c00ee90411191af5f648
Tkumar02 <ta_265@yahoo.co.uk>
2023-08-13 08:16:08 +0100

    observable and subscription

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,23 +1,22 @@
-import { Component } from '@angular/core';
-import { PostService } from '../services/post.service';
-import { Post } from '../models/post';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs'
+
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  constructor(){}
 
-  posts: Array<any> = []
-
-  constructor(private postService:PostService){
-    this.posts = postService.postList;
-  }
-
-  addData(){
-    let num = this.posts.length + 1
-    let newPost:Post = {id: num, title:'post'+num}
-    this.postService.addPost(newPost)
+  ngOnInit(): void {
+    const obstest$ = new Observable (observer=>{
+      observer.next('printed from observable in Post Component');
+      observer.next(2)
+      observer.next(false)
+    }).subscribe(value=>{
+      console.log(value)
+    });
   }
 }
