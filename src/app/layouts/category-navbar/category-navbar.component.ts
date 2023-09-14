49dc527284a2d4db4042253fc7f1bdf6a581fde8
Tkumar02 <ta_265@yahoo.co.uk>
2023-09-14 15:31:18 +0100

    dynamic category navbar

--- src/app/layouts/category-navbar/category-navbar.component.ts
+++ src/app/layouts/category-navbar/category-navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-category-navbar',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-navbar.component.css']
 })
 export class CategoryNavbarComponent {
+
+  catArray: Array<any> = []
+
+  constructor(private catService: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.catService.loadData().subscribe(val=>{
+      this.catArray = val;
+      console.log(this.catArray)
+    })
+    
+  }
+
+
 
 }
